625802eb291dee6f69485e37c40ea6d093cca6a8
YIntong Zhang <zhangy35@rose-hulman.edu>
2022-03-08 08:37:33 -0500

    Initial Commit

---
+++ HelloWorld2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld2
+//
+//  Created by 章音童 on 2022/3/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
